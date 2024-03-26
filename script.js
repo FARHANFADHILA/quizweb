2ee36899650ac031e49247048d54945269a01e31
FARHANFADHILA <12250113444@students.uin-suska.ac.id>
2024-03-26 15:11:36 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelector('#bttn_1').addEventListener('click', function() {
+    // Mengubah warna latar belakang menjadi merah
+    document.body.style.backgroundColor = 'red';
+});
+
+document.querySelector('#bttn_2').addEventListener('click', function() {
+    // Mengubah warna latar belakang menjadi merah
+    document.body.style.backgroundColor = 'blue';
+});
+document.querySelector('#bttn_3').addEventListener('click', function() {
+    // Mengubah warna latar belakang menjadi merah
+    document.body.style.backgroundColor = 'yellow';
+});
